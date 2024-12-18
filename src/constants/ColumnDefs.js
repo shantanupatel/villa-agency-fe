4015b765553afdb0d06753829af1ae1820bf5070
Shantanu Patel <shantu.patel@gmail.com>
2024-12-18 12:26:55 +0530

    Column definitions added for Address List component

---
+++ src/constants/ColumnDefs.js
@@ -0,0 +1,39 @@
+export const AddressListColumnDefs = [
+  {
+    field: "id",
+    headerName: "ID",
+    maxWidth: 50,
+  },
+  {
+    field: "street",
+    headerName: "Street",
+    editable: true,
+    minWidth: 100,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    /* type: "number", */
+    editable: true,
+  },
+  {
+    field: "state",
+    headerName: "State",
+    editable: true,
+    maxWidth: 100,
+  },
+  {
+    field: "zip",
+    headerName: "Zip",
+    maxWidth: 100,
+  },
+  {
+    field: "country",
+    headerName: "Country",
+    maxWidth: 100,
+  },
+  {
+    field: "createdDate",
+    headerName: "Created Date",
+  },
+];
