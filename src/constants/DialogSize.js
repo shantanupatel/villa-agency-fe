28831f29b7afcbe65c7be15af8c04f2d2dfc87bf
Shantanu Patel <shantu.patel@gmail.com>
2024-12-18 12:26:02 +0530

    Constants added for DialogSize and Toast

---
+++ src/constants/DialogSize.js
@@ -0,0 +1,5 @@
+export const EXTRASMALL = "xs";
+export const SMALL = "sm";
+export const MEDIUM = "md";
+export const LARGE = "lg";
+export const EXTRALARGE = "xl";
---
+++ src/constants/ToastConfig.js
@@ -0,0 +1,26 @@
+export const ToastConfig = {
+  primary: {
+    label: "PRIMARY",
+    bg: "primary",
+  },
+  secondary: {
+    label: "SECONDARY",
+    bg: "secondary",
+  },
+  success: {
+    label: "SUCCESS",
+    bg: "success",
+  },
+  failure: {
+    label: "ERROR",
+    bg: "danger",
+  },
+  warn: {
+    label: "WARNING",
+    bg: "warning",
+  },
+  info: {
+    label: "INFORMATION",
+    bg: "info",
+  },
+};
