4dd237c8bb4d9cfe8610cd0f215b5de01dc700a9
Shantanu Patel <shantu.patel@gmail.com>
2024-12-18 12:30:29 +0530

    ToastComponent updated by removing unwanted elements for toast header.

--- src/Components/Toast/ToastComponent.js
+++ src/Components/Toast/ToastComponent.js
@@ -4,11 +4,6 @@ import Toast from "react-bootstrap/Toast";
 const ToastComponent = (props) => {
   return (
     <React.Fragment>
-      {/*
-      bg: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'dark' | 'light'
-      position: 'top-start' | 'top-center' | 'top-end' | 'middle-start' | 'middle-center' | 'middle-end' | 'bottom-start' | 'bottom-center' | 'bottom-end'
-       */}
-
       <Toast
         bg={props.bg ? props.bg : "light"}
         show={props.show}
@@ -16,9 +11,7 @@ const ToastComponent = (props) => {
         delay={props.delay ? props.delay : 10000}
         autohide={props.autohide ? props.autohide : false}>
         <Toast.Header>
-          {/* <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" /> */}
-          <strong className="me-auto">{props.header}</strong>
-          {/* <small>11 mins ago</small> */}
+          <span className="me-auto">{props.header}</span>
         </Toast.Header>
         <Toast.Body>{props.body}</Toast.Body>
       </Toast>
