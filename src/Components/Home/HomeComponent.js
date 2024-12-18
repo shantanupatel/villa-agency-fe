1dea04037e4607b336363ba6afe5a2e71738832e
Shantanu Patel <shantu.patel@gmail.com>
2024-12-18 09:56:35 +0530

    HomeComponent added

---
+++ src/Components/Home/HomeComponent.js
@@ -0,0 +1,29 @@
+import CarouselComponent from "../Carousel/CarouselComponent";
+import FeaturedComponent from "../Featured/FeaturedComponent";
+import FeaturedVideoComponent from "../Featured/FeaturedVideoComponent";
+import FunFactsComponent from "../FunFacts/FunFactsComponent";
+import DealsComponent from "../Deals/DealsComponent";
+import PropertiesComponent from "../Properties/PropertiesComponent";
+import ContactUsComponent from "../ContactUsComponent";
+
+const HomeComponent = () => {
+  return (
+    <>
+      <CarouselComponent />
+
+      <FeaturedComponent />
+
+      <FeaturedVideoComponent />
+
+      <FunFactsComponent />
+
+      <DealsComponent />
+
+      <PropertiesComponent />
+
+      <ContactUsComponent />
+    </>
+  );
+};
+
+export default HomeComponent;
