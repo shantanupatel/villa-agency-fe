36c41aeed8a04fc0f7dec574650fde62ea0751bd
Shantanu Patel <shantu.patel@gmail.com>
2024-12-19 15:47:49 +0530

    Login and Signup component updated with form data.

--- src/Components/Login/LoginComponent.js
+++ src/Components/Login/LoginComponent.js
@@ -1,5 +1,99 @@
-const LoginComponent = () => {
-  return <div>LoginComponent</div>;
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import ButtonComponent from "Components/UI/ButtonComponent";
+import { useNavigate } from "react-router-dom";
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
+const schema = Yup.object().shape({
+  username: Yup.string()
+    .min(1, "Must be at least 1 character or more.")
+    .required("Username is required"),
+  password: Yup.string()
+    .required("No password provided.")
+    .min(8, "Password is too short - should be 8 chars minimum.")
+    .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+});
+
+const LoginComponent = (props) => {
+  const history = useNavigate();
+
+  const handleSubmit = async (values) => {
+    console.log(values);
+
+    history("/admin/dashboard");
+  };
+
+  return (
+    <>
+      <div className="section-heading">
+        <Row>
+          <Col>
+            <h4>Login</h4>
+          </Col>
+        </Row>
+      </div>
+
+      <Row>
+        <Col sm={{ span: 6, offset: 3 }}>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={schema}
+            onSubmit={(values, { setSubmitting }) => {
+              handleSubmit(values);
+            }}>
+            {({ isSubmitting }) => (
+              <Form>
+                <div className="form-group">
+                  <Row>
+                    <Col md="4">
+                      <label htmlFor="username">Email</label>
+                    </Col>
+                    <Col md="8">
+                      <Field
+                        name="username"
+                        placeholder="Username"
+                        className="form-control"></Field>
+                      <ErrorMessage name="username" component="div" />
+                    </Col>
+                  </Row>
+                </div>
+
+                <div className="form-group">
+                  <Row>
+                    <Col md="4">
+                      <label htmlFor="password">Password</label>
+                    </Col>
+                    <Col md="8">
+                      <Field
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        className="form-control"></Field>
+                      <ErrorMessage name="password" component="div" />
+                    </Col>
+                  </Row>
+                </div>
+
+                <div className="form-group text-center">
+                  <ButtonComponent
+                    variant="primary"
+                    title="Submit"
+                    type="submit"
+                  />
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default LoginComponent;
--- src/Components/SignUp/SignUpComponent.js
+++ src/Components/SignUp/SignUpComponent.js
@@ -1,5 +1,149 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import ButtonComponent from "Components/UI/ButtonComponent";
+import { useNavigate } from "react-router-dom";
+
+const initialValues = {
+  fullname: "",
+  email: "",
+  password: "",
+  // role: "",
+};
+
+const schema = Yup.object().shape({
+  fullname: Yup.string()
+    .min(1, "Must be at least 1 character or more.")
+    .required("Full name is required."),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email address is required."),
+  password: Yup.string()
+    .required("No password provided.")
+    .min(8, "Password is too short - should be 8 chars minimum.")
+    .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+  // role: Yup.string().required("Role is required."),
+});
+
 const SignUpComponent = () => {
-  return <div>SignUpComponent</div>;
+  const history = useNavigate();
+
+  const handleSubmit = async (values) => {
+    console.log(values);
+
+    history("/admin");
+  };
+
+  /* function handleCityChange(e) {
+    setSelectedCity(selectedCity);
+
+    handleChange(e);
+  } */
+
+  return (
+    <>
+      <div className="section-heading">
+        <Row>
+          <Col>
+            <h4>Login</h4>
+          </Col>
+        </Row>
+      </div>
+
+      <Row>
+        <Col sm={{ span: 6, offset: 3 }}>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={schema}
+            onSubmit={(values) => {
+              handleSubmit(values);
+            }}>
+            {({ isSubmitting, handleChange }) => (
+              <Form>
+                <div className="form-group">
+                  <Row>
+                    <Col md="4">
+                      <label htmlFor="fullname">Full Name</label>
+                    </Col>
+                    <Col md="8">
+                      <Field
+                        name="fullname"
+                        placeholder="Full Name"
+                        className="form-control"></Field>
+                      <ErrorMessage name="fullname" component="div" />
+                    </Col>
+                  </Row>
+                </div>
+
+                <div className="form-group">
+                  <Row>
+                    <Col md="4">
+                      <label htmlFor="email">Email</label>
+                    </Col>
+                    <Col md="8">
+                      <Field
+                        name="email"
+                        placeholder="Email address"
+                        className="form-control"></Field>
+                      <ErrorMessage name="email" component="div" />
+                    </Col>
+                  </Row>
+                </div>
+
+                <div className="form-group">
+                  <Row>
+                    <Col md="4">
+                      <label htmlFor="password">Password</label>
+                    </Col>
+                    <Col md="8">
+                      <Field
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        className="form-control"></Field>
+                      <ErrorMessage name="password" component="div" />
+                    </Col>
+                  </Row>
+                </div>
+
+                {/* <div className="form-group">
+                  <Row>
+                    <Col md="4">
+                      <label htmlFor="role">Role</label>
+                    </Col>
+                    <Col md="8">
+                      <Field
+                        as="select"
+                        name="role"
+                        className="form-control">
+                        {!city && <option key="0">Select Role</option>}
+                        {cities &&
+                          cities.map((city) => (
+                            <option value={city.isoCode} key={city.isoCode}>
+                              {city.name}
+                            </option>
+                          ))}
+                      </Field>
+                      <ErrorMessage name="password" component="div" />
+                    </Col>
+                  </Row>
+                </div> */}
+
+                <div className="form-group text-center">
+                  <ButtonComponent
+                    variant="primary"
+                    title="Submit"
+                    type="submit"
+                  />
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default SignUpComponent;
