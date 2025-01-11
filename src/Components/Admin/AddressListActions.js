fe80a79dd824c04865027c950aee1eeb1de57b56
Shantanu Patel <shantu.patel@gmail.com>
2025-01-11 12:19:47 +0530

    ColumnDefs for AddressList updated along with getting data from grid actions to handle edit and delete.

---
+++ src/Components/Admin/AddressListActions.js
@@ -0,0 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { Stack } from "react-bootstrap";
+
+export const AddressListActions = (params) => {
+  // console.log(params);
+
+  const handleClickEvent = (data) => {
+    params.clicked(data);
+  };
+
+  return (
+    <>
+      <Stack direction="horizontal" gap={3}>
+        <div>
+          <FontAwesomeIcon
+            icon={faPenToSquare}
+            title="Edit"
+            onClick={() =>
+              handleClickEvent({
+                id: params.data.id,
+                event: "edit",
+              })
+            }
+          />
+        </div>
+        <div>
+          <FontAwesomeIcon
+            icon={faTrashCan}
+            title="Delete"
+            onClick={() =>
+              handleClickEvent({
+                id: params.data.id,
+                event: "delete",
+              })
+            }
+          />
+        </div>
+      </Stack>
+    </>
+  );
+};
