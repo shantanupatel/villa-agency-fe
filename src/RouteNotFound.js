2780cfdc34c7098814954b2eb02d4d09e4548863
Shantanu Patel <shantu.patel@gmail.com>
2025-01-15 16:10:07 +0530

    Fixed countdown logic issue for RouteNotFound component.

--- src/RouteNotFound.js
+++ src/RouteNotFound.js
@@ -1,22 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouteError, useNavigate } from "react-router-dom";
 
 const RouteNotFound = ({ timeoutSeconds }) => {
   const error = useRouteError();
   const navigate = useNavigate();
+  const [timeRemaining, setTimeRemaining] = useState(5);
+
   useEffect(() => {
-    setTimeout(() => {
+    /* setTimeout(() => {
       navigate("");
-    }, timeoutSeconds * 1000);
+    }, timeoutSeconds * 1000); */
+
+    const countdownInterval = setInterval(() => {
+      updateTimer();
+
+      if (timeRemaining === 0) {
+        navigate("");
+      }
+    }, 1000);
+
+    return () => clearInterval(countdownInterval);
   });
+
+  const updateTimer = () => {
+    setTimeRemaining((prevValue) => prevValue - 1);
+  };
 
   return (
     <div className="container text-center">
       <h1>404</h1>
       <p>Sorry, we couldn't find that page.</p>
       <p>
-        Redirecting in {timeoutSeconds}{" "}
-        {timeoutSeconds > 1 ? "seconds" : "second"}
+        Redirecting in {timeRemaining}{" "}
+        {timeRemaining > 1 ? "seconds" : "second"}
         <i>{JSON.parse(error)}</i>
       </p>
     </div>
